8b8c016ae485722d293f962e3ed9cc509aac238a
Maxim Kasyanenko <maxim.kasyanenko@protonmail.ch>
2021-08-12 14:26:46 -0700

    custom config; update README

--- config.h
+++ config.h
@@ -1,20 +1,35 @@
 /* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
+/* Custom settings inline with my dwm build. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Noto Mono:size=14"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* -p  option; prompt to the left of input field */
+static const char *prompt = NULL; 
+
+/* colors based on dwm build */
+static const char clr_black[]       = "#000000";
+static const char clr_gray []       = "#808080";
+static const char clr_white[]       = "#ffffff";
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*               fg         bg         */
+	[SchemeNorm] = { clr_gray , clr_black },
+	[SchemeSel]  = { clr_white, clr_black },
+
+	/* this color doesn't seem to be used with an
+	 * unpatched dmenu.
+	 */
+	[SchemeOut]  = { "#000000", "#00ffff" },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
